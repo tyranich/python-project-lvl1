5b4e24c95adacec10784f33a6218cc3e067fc352
tyranich <tyranich@mail.ru>
2021-12-03 00:52:58 +0300

    add brain_games.py

---
+++ brain_games/scripts/games/brain_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import prompt
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print("Hello, {}!".format(name))
+
+
+if __name__ == "__main__":
+    main()
