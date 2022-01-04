ea79b7532b3ee1ba16dda35a6d25051657b06774
tyranich <tyranich@mail.ru>
2022-01-05 01:16:43 +0300

    test

--- brain_games/scripts/for_engine/engine.py
+++ brain_games/scripts/for_engine/engine.py
@@ -2,30 +2,30 @@ import prompt
 import sys
 
 
-def welcome_user():
+def engine_for_games(greeting, create_question):
+
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
-    print("Hello, {}!".format(name))
-    return name
+    print("Hello, {}!".format(name)) 
+    print(greeting)
+    
+    while i < 3:
+        question, correct_ans = create_question()
+        print("Question: {}".format(question))
+        answer = prompt.string("Your answer: ")
+        if answer == correct_ans:
+            i += 1
+            print("Correct!")
+        elif answer != correct_ans:
+            string_wrong_ans = "'{}' is wrong answer ;(. Correct answer was '{}'".format(answer, correct_ans)
+            print("Let's try again, {}!".format(name))
+            sys.exit(0)
+
+        #elif answer != correct_val:
+         #   print(string_wrong_ans.format(answer, correct_val))
+        
 
 
-def question_for_user(string_for_question):
-    print("Question: {}".format(string_for_question))
-    answer = prompt.string("Your answer: ")
-    return answer
-
-
-def cheking_answer(answer, correct_val, name):
-    string_wrong_ans = "'{}' is wrong answer ;(. Correct answer was '{}'"
-    if answer == correct_val:
-        print("Correct!")
-    elif answer != correct_val:
-        print(string_wrong_ans.format(answer, correct_val))
-        print("Let's try again, {}!".format(name))
-        sys.exit(0)
-
-
-def farewell(name):
     print("Congratulations, {}!".format(name))
 
 
--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -2,27 +2,17 @@
 from brain_games.scripts.for_engine import engine
 import random
 
+greeting = "What is the result of the expression?"
 
 def main():
 
-    name = engine.welcome_user()
-    print("What is the result of the expression?")
     sings_collection = ("-", "+", "*")
-    i = 0
-    while True:
-        num = random.randint(1, 10)
-        second_num = random.randint(1, 10)
-        random_sing = random.choice(sings_collection)
-        string_for_question = "{} {} {}".format(num, random_sing, second_num)
-        correctly_answer = int(eval(string_for_question))
-        answer = engine.question_for_user(string_for_question)
-        engine.cheking_answer(answer, str(correctly_answer), name)
-        i += 1
-        if i == 3:
-            engine.farewell(name)
-            break
-
+    num = random.randint(1, 10)
+    second_num = random.randint(1, 10)
+    random_sing = random.choice(sings_collection)
+    string_for_question = "{} {} {}".format(num, random_sing, second_num)
+    correctly_answer = int(eval(string_for_question))
+    return string_for_question, correctly_answer
 
 if __name__ == "__main__":
-
-    main()
+    engine.engine_for_games(greeting, main())
