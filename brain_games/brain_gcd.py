bd08c688c93e7ecf5a88eda8e1e7566e5134e1b6
tyranich <tyranich@mail.ru>
2022-02-08 23:35:21 +0300

    the end

--- brain_games/brain_gcd.py
+++ brain_games/brain_gcd.py
@@ -1,13 +1,18 @@
 import random
-import math
 
 
 GREETING = "Find the greatest common divisor of given numbers."
+
+
+def gcd(num_1, num_2):
+    if num_1 == 0:
+        return num_2
+    return gcd(num_2 % num_1, num_1)
 
 
 def generate_round():
     first_num = random.randint(1, 100)
     second_num = random.randint(1, 100)
     string_for_question = "{} {}".format(first_num, second_num)
-    correctly_answer = math.gcd(first_num, second_num)
+    correctly_answer = gcd(first_num, second_num)
     return string_for_question, str(correctly_answer)
